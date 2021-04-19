3b0d736e901d62fb62ef162205f97295f67dea16
Koriukina, Valeriia <valeriia.koriukina@xperience.ai>
2021-04-19 17:48:09 +0300

    updated __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,6 +6,9 @@ def get_datasets(opts):
         trainset = chestxray14_dataset.ChestXray14_Train(opts)
         valset = chestxray14_dataset.ChestXray14_Vali(opts)
         testset = chestxray14_dataset.ChestXray14_Test(opts)
+    elif opts.test_list_box:
+        testset = chestxray14_dataset.ChestXray14_Test(opts)
+        return None, None, testset
 
     else:
         raise NotImplementedError
